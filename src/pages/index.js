4338f04319c459f2defa4fded821939511e3d263
Cristian Gabbanini <cristian@begenius.it>
2019-04-30 19:34:26 +0200

    Adds source code up to step4/mdx-blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import usePosts from '../hooks/usePosts';
+import PostPreview from '../components/post-preview';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Minnesota!</p>
+      <Link to="/about/">Learn about me →</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
